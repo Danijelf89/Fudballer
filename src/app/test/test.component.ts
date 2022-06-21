b520813d53feca20cc4fba848a585b36be156026
danijelf89@hotmail.com <danijelf89@hotmail.com>
2022-06-21 13:51:26 +0200

    StandingsAdded
    -standings table added
    - standings list view added

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { ToastrService } from 'ngx-toastr';
+import { Club } from '../Clubs/club';
+import { IFootballers } from '../Footballers/footballers.model';
+import { FootballersService } from '../Footballers/footballers.service';
 import { Playersmodel } from '../playersmodel';
 
 @Component({
@@ -10,21 +15,59 @@ import { Playersmodel } from '../playersmodel';
 })
 export class TestComponent implements OnInit {
 
-  constructor(private toastr: ToastrService) { }
+  constructor(private toastr: ToastrService, public service: FootballersService) { }
 
   pointsSum: number = 0;
+  clubs: Club[] = [];
+  Playersmodel : Playersmodel[] = [];
+  showHideStandings : boolean = true;
+
+
+  displayedColumnsBase = ['name', 'surname', 'toalPoints'];
+  dataSourceBase = new MatTableDataSource<any>(); 
+  @ViewChild('paginator') paginator!: MatPaginator;
+
 
   ngOnInit(): void {
+   
+
+
+    this.Playersmodel = [{
+      id:1,
+      name : 'Aneta',
+      surname : "Cebovic",
+      points : 0,
+      pointsSum : 0,
+      pictureUrl : "https://problemparrots.co.uk/wp-content/uploads/Budgie.jpg"
+    },
+    {
+      id:2,
+      name : 'Danijel',
+      surname : "Filipovic",
+      points : 0,
+      pointsSum : 0,
+      pictureUrl : "https://problemparrots.co.uk/wp-content/uploads/Budgie.jpg"
+    }
+  
+  ];
+
+    this.dataSourceBase.data = this.Playersmodel;
+
+    console.log("datasource", this.dataSourceBase);
   }
+
+  ngAfterViewInit(){
+    this.dataSourceBase.data = this.Playersmodel;
+  }
+
+ 
+
 
   payerForm = new FormGroup({
     points: new FormControl(0,[Validators.required, Validators.min(1)]),
   });
 
-  Playersmodel : Playersmodel[] = [];
-
-
-
+  
 
 
 
@@ -42,6 +85,8 @@ export class TestComponent implements OnInit {
     }
   }
 
+  
+
   addNewPlayer(){
     this.Playersmodel.push({
       id:1,
@@ -51,6 +96,8 @@ export class TestComponent implements OnInit {
       pointsSum : 0,
       pictureUrl : "https://problemparrots.co.uk/wp-content/uploads/Budgie.jpg"
     });
+
+    this.dataSourceBase = new MatTableDataSource(this.Playersmodel);
   }
 
 }
